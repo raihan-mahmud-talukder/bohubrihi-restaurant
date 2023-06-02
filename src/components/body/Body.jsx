350f9736b4dd75ab8343972dd97095f01e39202d
raihan-mahmud-talukder <raihan_uits@outlook.com>
2023-06-02 14:17:32 +0600

    adding router

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import { About } from "./About"
+import { Contact } from "./Contact"
+import { Home } from "./Home"
 import { Menu } from "./Menu"
 
 export const Body = () => {
     return (
-        <>
-        <Menu />
-        </>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+        </Routes>
     )
     }
